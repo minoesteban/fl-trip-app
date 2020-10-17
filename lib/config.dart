36d2b560103afd923674e1a890f7bc53d0a2dc67
Esteban Miño <65094540+minoesteban@users.noreply.github.com>
2020-10-17 20:15:54 -0300

    Delete config.dart

--- lib/config.dart
+++
@@ -1,5 +0,0 @@
-const API_BASE_URL = '/test/api/1.0';
-const API_HOST = 'https://cgapxxqbp1.execute-api.us-east-1.amazonaws.com';
-const API_HOST_ANDROID = API_HOST;
-const API_ENDPOINT = '$API_HOST$API_BASE_URL';
-const API_ENDPOINT_ANDROID = '$API_HOST_ANDROID:$API_BASE_URL';
